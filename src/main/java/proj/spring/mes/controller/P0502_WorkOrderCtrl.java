2acc1ef15737f23ab90f6dd07ee41e4fd510cfa3
SeongYeonYun <69850705+SeongYeonYun@users.noreply.github.com>
2025-10-17 16:33:09 +0900

    작업지시서 workorder

--- src/main/java/proj/spring/mes/controller/P0502_WorkOrderCtrl.java
+++ src/main/java/proj/spring/mes/controller/P0502_WorkOrderCtrl.java
@@ -1,5 +1,44 @@
 package proj.spring.mes.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import proj.spring.mes.dto.ItemDTO;
+import proj.spring.mes.dto.P0501_CPDTO;
+import proj.spring.mes.service.P0501_CPService;
+import proj.spring.mes.service.P0502_WorkOrderService;
+
+@Controller
 public class P0502_WorkOrderCtrl {
+	
+	
+	@Autowired
+	P0502_WorkOrderService WorkOrderService;
+	
+	
+	/** 목록 */
+	@RequestMapping("/workorder")
+    public String list() {
+        
+
+        
+        System.out.println("목록페이지");
+        
+        
+        return "05_production/05_2_workorder.tiles"; 
+	
+	
+	
+	
+	
+	
 
 }
+	}
