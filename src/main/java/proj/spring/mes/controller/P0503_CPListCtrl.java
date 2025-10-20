ced931de67a5392d4463b5746ed99d159cd8df58
최아라 <a0420ra@gmail.com>
2025-10-20 09:56:54 +0900

    생산실적주소연결

--- src/main/java/proj/spring/mes/controller/P0503_CPListCtrl.java
+++ src/main/java/proj/spring/mes/controller/P0503_CPListCtrl.java
@@ -1,5 +1,26 @@
 package proj.spring.mes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import proj.spring.mes.service.P0503_CPListService;
+
+@Controller
 public class P0503_CPListCtrl {
 
+	@Autowired
+	P0503_CPListService CPListService;
+	
+	
+	/** 목록 */
+	@RequestMapping("/cplist")
+    public String list() {
+        
+
+        System.out.println("목록페이지");
+        
+        return "05_production/05_3_cplist.tiles"; 
+	}
+	
 }
